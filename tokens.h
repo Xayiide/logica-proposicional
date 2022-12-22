cff1a1f279c8ffbcfc551b913a1be65b033c82a6
Xayiide <wickedcheerio@protonmail.com>
2022-12-23 00:36:45 +0100

    Parser léxico. En camino el sintáctico

---
+++ tokens.h
@@ -0,0 +1,10 @@
+
+#define VAR_PROP     260
+#define OP_COND      261
+#define OP_BICOND    262
+#define OP_DISY      263
+#define OP_CONJ      264
+#define OP_NEG       265
+#define PAR_APER     266
+#define PAR_CIER     267
+
